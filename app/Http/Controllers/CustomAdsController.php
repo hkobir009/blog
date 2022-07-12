29a864b8660d6c0e06f076caaed8ca54bfeb2b41
Humayun <hkobir009@gmail.com>
2022-07-12 10:07:03 +0600

    created all model controller request and migration files

---
+++ app/Http/Controllers/CustomAdsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomAds;
+use Illuminate\Http\Request;
+
+class CustomAdsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CustomAds  $customAds
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CustomAds $customAds)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CustomAds  $customAds
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CustomAds $customAds)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CustomAds  $customAds
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CustomAds $customAds)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CustomAds  $customAds
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CustomAds $customAds)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SocialUrlController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SocialUrl;
+use Illuminate\Http\Request;
+
+class SocialUrlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SocialUrl  $socialUrl
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SocialUrl $socialUrl)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SocialUrl  $socialUrl
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SocialUrl $socialUrl)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SocialUrl  $socialUrl
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SocialUrl $socialUrl)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SocialUrl  $socialUrl
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SocialUrl $socialUrl)
+    {
+        //
+    }
+}
